b7cfaad5b6c5d5b34cdd18ea9291284b64d96f80
Winston White <whitew2015@fau.edu>
2024-10-16 18:25:07 -0400

    added functionality to enter button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 const userInput = document.getElementById("text-input");
-const resultButton = document.getElementById("check-btn");
+const checkButton = document.getElementById("check-btn");
 const resultText = document.getElementById("result");
+
+const checkPalindrome = () => {
+    if(userInput === '') {
+        alert("Enter any text!");
+        return;
+    } 
+    resultText.textContent = "";
+    const reverseText = userInput.split(' ').reverse().join('');
+
+    if (userInput === reverseText) {
+        resultText.textContent = '${userInput} is a palindrome.';
+    }  else {
+        resultText.textContent = '${UserInput} is not a plaindrome.';
+    }
+};
+
+
+checkButton.addEventListener("click", checkPalindrome);
